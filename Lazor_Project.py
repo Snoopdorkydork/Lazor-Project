903be7c1cb1a066adf3bb0bf67e1c598706dce38
Snoopdorkydork <93001629+Snoopdorkydork@users.noreply.github.com>
2021-11-11 10:47:02 -0500

    Add files via upload

---
+++ Lazor_Project.py
@@ -0,0 +1,12 @@
+# This is this initial commit to GitHub for the Lazor Project
+class Block:
+	def __init__(self):
+		self.type = 
+		self.pos = 
+
+	def action(self):
+		self.face = self.pos...
+		
+
+
+if __name__ == '__main__'
